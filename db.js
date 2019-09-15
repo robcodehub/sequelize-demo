d90463b2ba8184b639bfa60c757334086fc81157
Robert Wise <robcodehub@gmail.com>
2019-09-15 19:09:40 +0200

    added environment database_url

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const conn = new Sequelize('postgres://localhost/sequelize_demo_db');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/sequelize_demo_db');
 
 const Product = conn.define('product', {
   id: {
