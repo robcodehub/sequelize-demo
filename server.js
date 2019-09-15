f69c7e732f659d0869d313a7426000e047b5bbc8
Robert Wise <robcodehub@gmail.com>
2019-09-15 19:08:06 +0200

    added environment port for deployment

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use('/api', require('./api'));
 
 
 
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 db.syncAndSeed()
 .then(() => app.listen(port, () => console.log(`listening on port ${port}`)));
